5b59590821541552af1257a0ad1e2ad5fe196a07
PARK-SSOYEON <soyeon00307@gmail.com>
2025-11-04 15:02:09 +0900

    feat: Input component

--- apps/user-web/src/App.tsx
+++ apps/user-web/src/App.tsx
@@ -1,7 +1,18 @@
+import * as React from 'react';
 import "./index.css";
-import {Button} from "@dash/ui";
+import {Button, Input} from "@dash/ui";
 
 function App() {
+    const [value, setValue] = React.useState("");
+    const [error, setError] = React.useState<string | undefined>();
+
+    const submit = () => {
+        if (!value.trim()) {
+            // 에러 플래그 던지기
+            setError("필수 입력값이에요");
+        }
+    };
+
     return (
         <div className="flex w-full flex-col">
             <Button
@@ -14,11 +25,21 @@ function App() {
                 size="md"
                 icon={"identify"}
             > 테스트용 </Button>
-            <Button
-                mode="color_line"
-                size="md"
-                icon={"identify"}
-            > 테스트용 </Button>
+            <div className="flex flex-row items-center">
+                <Button
+                    mode="color_line"
+                    size="md"
+                    icon={"identify"}
+                > 테스트용 </Button>
+
+                <Input
+                    label="이름"
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                    errorMessage={error}
+                />
+            </div>
+
             <Button
                 mode="color_line"
                 size="sm"
@@ -29,6 +50,10 @@ function App() {
                 size="sm"
                 icon={"rightArrow"}
             />
+            <Input label="입력제목" defaultValue="대충 입력값..."/>
+            <Input label="입력제목"/>
+            <button onClick={submit}>제출</button>
+
         </div>
     );
 }
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,5 +2,5 @@
 export * from "./components/Button";
 export * from "./icons/IconRegistry"
 export * from "./components/Icon";
-//export * from "./components/Input";
+export * from "./components/Input";
 //export * from "./components/modal/Modal";
